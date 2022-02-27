01eb8123d56f962ff4819ed7f1d07be6eb72ef31
rsodxd <thesourcecatt@outlook.com>
2022-02-27 11:42:48 +0800

    Signed-off-by: rsodxd <thesourcecatt@outlook.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ export default function Footer() {
 	return (
 		<div className="footer section">
 			<div className="section-content footer-content">
-				<div className="title-group">
+				<div className="title-group" style={{ color: '#FFFFFF' }} >
 					<h1 className="subtitle">Webpage designed by rsodxd & LiChen.</h1>
 				</div>
 			</div>
-		</div>
+		</div >
 	);
 }
--- src/pages/Softwares/Jiyu.js
+++ src/pages/Softwares/Jiyu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import JiyuTaken1 from '../../images/JiyuTaken/1.png';
-import JiyuTaken2 from '../../images/JiyuTaken/2.png';
+import JiyuTakenVid1 from '../../images/JiyuTaken/1.mp4'
 
 export default function Jiyu() {
 	return (
@@ -22,13 +22,17 @@ export default function Jiyu() {
 					</div>
 				</a>
 			</section>
-			<section className="section section-hero" style={{ backgroundImage: 'url(' + JiyuTaken2 + ')' }}>
-				<a className="section-content section-content-mini" href="/unready">
-					<div className="title-group" style={{ color: '#ffffff' }}>
-						<h1 className="title">观看宣传片</h1>
-						<h1 className="subtitle">用之前先看一眼。</h1>
-					</div>
-				</a>
+			<section className="section verticle">
+				<div className="title-group" style={{ color: 'rgb(0, 56, 90)' }}>
+					<h1 className="title">集与一身</h1>
+					<h1 className="subtitle">方便快捷。</h1>
+				</div>
+				<div className='media-content'>
+					<video src={JiyuTakenVid1} autoPlay loop muted></video>
+				</div>
+				<div className="title-group" style={{ color: 'rgb(0, 56, 90)' }}>
+					<h1 className="subtitle">inset text</h1>
+				</div>
 			</section>
 		</React.Fragment>
 	);
