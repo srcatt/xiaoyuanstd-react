69d2ea5c064d837650df58faec9605b35f4db122
rsodxd <thesourcecatt@outlook.com>
2022-02-27 14:41:17 +0800

    Signed-off-by: rsodxd <thesourcecatt@outlook.com>

--- src/App.js
+++ src/App.js
@@ -12,15 +12,17 @@ import Malwares from './pages/Malwares';
 import LCVirus from './pages/Malwares/LCVirus';
 import Rick from './images/Rick.mp4';
 
-console.log("\n每一个星球都有一个驱动核心,",
-"\n每一种思想都有影响力的种子",
-"\n感受世界的温度,",
-"\n年轻的你也能成为改变世界的动力,",
-"\n我们珍惜你所有的潜力",
-"\n你的潜力,",
-"是改变世界的动力!\n\n");
-console.log("\n%c小源工作室2022招聘简历投递: https://wj.qq.com/s2/9765478/b490","color:red");
 export default function App() {
+	console.log(
+		'\n每一个星球都有一个驱动核心,',
+		'\n每一种思想都有影响力的种子',
+		'\n感受世界的温度,',
+		'\n年轻的你也能成为改变世界的动力,',
+		'\n我们珍惜你所有的潜力',
+		'\n你的潜力,是改变世界的动力!'
+	);
+	console.log('\n%c小源工作室招聘问卷：https://wj.qq.com/s2/9765478/b490', 'color: red');
+	console.log('\n%c感谢百度', 'text-decoration: line-through');
 	return (
 		<React.Fragment>
 			<Navbar />
@@ -41,10 +43,11 @@ export default function App() {
 									<h1 className="title">该内容尚未准备。</h1>
 									<h1 className="subtitle">该网站处于开发阶段，请等待程序猿把这个坑填平。</h1>
 									<h1 className="subtitle">
-										开发进度可在<a
-										 href="/rick" 
-										 className='a_decoration'
-										 style={{ color : 'orange' }}>这里</a>查看。
+										开发进度可在
+										<a href="/rick" className="a_decoration" style={{ color: 'orange' }}>
+											这里
+										</a>
+										查看。
 									</h1>
 								</div>
 							}
--- src/pages/Softwares/Jiyu.js
+++ src/pages/Softwares/Jiyu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import JiyuTaken1 from '../../images/JiyuTaken/1.png';
-import JiyuTakenVid1 from '../../images/JiyuTaken/1.mp4'
+import JiyuTakenVid1 from '../../images/JiyuTaken/1.mp4';
 
 export default function Jiyu() {
 	return (
@@ -27,11 +27,11 @@ export default function Jiyu() {
 					<h1 className="title">集与一身</h1>
 					<h1 className="subtitle">方便快捷。</h1>
 				</div>
-				<div className='media-content'>
+				<div className="media-content">
 					<video src={JiyuTakenVid1} autoPlay loop muted></video>
 				</div>
 				<div className="title-group" style={{ color: 'rgb(0, 56, 90)' }}>
-					<h1 className="subtitle">inset text</h1>
+					<h1 className="subtitle">insert text</h1>
 				</div>
 			</section>
 		</React.Fragment>
