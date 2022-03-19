c7821bba2995222a603d6d4dddfef9cb07622117
rsodxd <thesourcecatt@outlook.com>
2022-03-19 18:06:39 +0800

    Signed-off-by: rsodxd <thesourcecatt@outlook.com>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Jiyu from './pages/Softwares/Jiyu';
 import JiyuBC from './pages/Softwares/JiyuBC';
 import TaskMgrPlus from './pages/Softwares/TaskMgrPlus';
 import Malwares from './pages/Malwares';
-import Join from './pages/Join';
 
 export default function App() {
 	console.log(
