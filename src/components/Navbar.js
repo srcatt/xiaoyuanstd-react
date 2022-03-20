c6da0bacec6677d49491f47b24e4a05815641b7e
rsodxd <thesourcecatt@outlook.com>
2022-03-20 13:46:12 +0800

    Signed-off-by: rsodxd <thesourcecatt@outlook.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
-import { React } from 'react';
+import React from 'react';
 import Apps from '@mui/icons-material/Apps';
 import BugReport from '@mui/icons-material/BugReport';
 import GitHub from '@mui/icons-material/GitHub';
 import Menu from '@mui/icons-material/Menu';
+import People from '@mui/icons-material/People';
 
 export default function Navbar() {
 	function ExpandDrawer() {
@@ -27,6 +28,10 @@ export default function Navbar() {
 				<a className="navigation-btn" href="/malwares">
 					<BugReport />
 					病毒
+				</a>
+				<a className="navigation-btn" href="https://wj.qq.com/s2/9765478/b490" target="_blank" rel="noreferrer">
+					<People />
+					加入我们
 				</a>
 				<a className="navigation-btn" href="https://github.com/xystd" target="_blank" rel="noreferrer">
 					<GitHub />
