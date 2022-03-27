79efe232cb5138cf05ff2a0323b229f506f7aae8
rsodxd <thesourcecatt@outlook.com>
2022-03-27 14:10:30 +0800

    Signed-off-by: rsodxd <thesourcecatt@outlook.com>

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,14 +1,10 @@
-export default function TeamCard(props) {
+export default function TeamCard({ avatar, name, role }) {
 	return (
 		<div className="team-card">
-			<img
-				className="team-card-img"
-				src={'https://q1.qlogo.cn/g?b=qq&nk=' + props.avatar + '&s=640'}
-				alt={props.name}
-			></img>
+			<img className="team-card-img" src={'https://q1.qlogo.cn/g?b=qq&nk=' + avatar + '&s=640'} alt={name}></img>
 			<div className="title-group">
-				<h1 className="title team-title">{props.name}</h1>
-				<h1 className="subtitle team-subtitle">{props.role}</h1>
+				<h1 className="title team-title">{name}</h1>
+				<h1 className="subtitle team-subtitle">{role}</h1>
 			</div>
 		</div>
 	);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import TeamCard from '../components/TeamCard';
 import Hero from '../images/JiyuTaken/1.png';
+import OpenSource from '../images/open-source.png';
 
 export default function Home() {
 	return (
 		<React.Fragment>
+			<Alert severity="info">
+				<AlertTitle>向所有在东航MU5735航班中遇难的人们默哀。</AlertTitle>
+				网站现已黑白。
+			</Alert>
 			<section className="section section-hero" style={{ backgroundImage: 'url(' + Hero + ')' }}>
 				<a className="section-content" href="/softwares">
 					<div className="title-group" style={{ color: '#ffffff' }}>
@@ -16,7 +23,7 @@ export default function Home() {
 			<section className="section verticle" id="#about">
 				<div className="title-group">
 					<h1 className="title">关于我们</h1>
-					<h1 className="subtitle">排序不分前后</h1>
+					<h1 className="subtitle">一群程序员，做我们喜爱的东西。</h1>
 				</div>
 				<div className="row">
 					<TeamCard avatar="3627046040" name="小源151" role="室长" />
@@ -24,8 +31,21 @@ export default function Home() {
 					<TeamCard avatar="2216878969" name="Leo" role="ohhhhhh" />
 					<TeamCard avatar="2952558045" name="rsodxd" role="全栈程序员" />
 					<TeamCard avatar="2156391386" name="Chils" role="软件开发" />
-					<TeamCard avatar="2991266101" name="copi143 | 无名氏" role="软件开发" />
+					<TeamCard avatar="2991266101" name="copi143" role="软件开发" />
 				</div>
+			</section>
+			<section className="section section-hero" style={{ backgroundImage: 'url(' + OpenSource + ')' }}>
+				<a
+					className="section-content section-content-mini"
+					href="https://github.com/xystd"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="title-group" style={{ color: '#ffffff' }}>
+						<h1 className="title">做开源的事</h1>
+						<h1 className="subtitle">部分源码开源在Github。</h1>
+					</div>
+				</a>
 			</section>
 		</React.Fragment>
 	);
